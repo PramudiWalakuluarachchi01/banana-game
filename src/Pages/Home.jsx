846cc94d25e7b8d766da0f5778b961f873056409
rZoysa <rithikzoysa@gmail.com>
2024-11-16 19:23:15 +0530

    wrapped account image in motion div

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,69 +1,68 @@
 import { Link } from "react-router-dom";
-import { motion } from "motion/react"
-
+import { motion } from "motion/react";
 
 const Home = () => {
   return (
     <div className="flex justify-center items-center h-screen w-screen text-black">
-    <div className="relative bg-[#A8A8A87A] bg-opacity-45 rounded-3xl w-7/12 h-4/6 flex justify-center items-center">
-       
-    <ul className="list-disc text-6xl font-itim font-bold text-black ml-10 inline-block select-none">
-      <li className="transition-all hover:scale-125 m-7 w-fit ">
-                <motion.div
-                  className="box text-4xl underline"
-                  whileHover={{ scale: 1.1 }}
-                  transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                >
-                  <Link to="/difficulty">Play Now</Link>
-                  </motion.div>
-        </li>
+      <div className="relative bg-[#A8A8A87A] bg-opacity-45 rounded-3xl w-7/12 h-4/6 flex justify-center items-center">
+        <ul className="list-disc text-6xl font-itim font-bold text-black ml-10 inline-block select-none">
+          <li className="transition-all hover:scale-125 m-7 w-fit ">
+            <motion.div
+              className="box text-4xl underline"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
+              <Link to="/difficulty">Play Now</Link>
+            </motion.div>
+          </li>
 
-      <li className="transition-all hover:scale-125 m-7 w-fit">
-                <motion.div
-                  className="box text-4xl underline"
-                  whileHover={{ scale: 1.1 }}
-                  transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                >
-                  <Link to="/ScoreBoard">Score Board</Link>
-                  </motion.div>
-          
-      </li> 
-        <li className="transition-all hover:scale-125 m-7 w-fit">
-                <motion.div
-                  className="box text-4xl underline"
-                  whileHover={{ scale: 1.1 }}
-                  transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                >
-                  <Link to="/Instrctions">Instructions</Link>
-                  </motion.div>
-        </li>
- 
-       </ul>
-       
-        </div>   
-        <div className="absolute top-40 right-96 mt-6 mr-5">
-        <Link to="/User">
-          <img src="account1.png" alt="" />
-        </Link>
+          <li className="transition-all hover:scale-125 m-7 w-fit">
+            <motion.div
+              className="box text-4xl underline"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
+              <Link to="/ScoreBoard">Score Board</Link>
+            </motion.div>
+          </li>
+          <li className="transition-all hover:scale-125 m-7 w-fit">
+            <motion.div
+              className="box text-4xl underline"
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
+              <Link to="/Instrctions">Instructions</Link>
+            </motion.div>
+          </li>
+        </ul>
+      </div>
+      <div className="absolute top-40 right-96 mt-6 mr-5">
+        <motion.div
+          className="box"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ type: "spring", stiffness: 400, damping: 17 }}
+        >
+          <Link to="/User">
+            <img src="account1.png" alt="" />
+          </Link>
+        </motion.div>
       </div>
 
-        <div className="absolute top-44 left-28 mt-6 ml-80">
+      <div className="absolute top-44 left-28 mt-6 ml-80">
         <Link to="/">
           <img src="back.png" alt="" />
         </Link>
       </div>
-      
-<img
+
+      <img
         src="pic3.png"
         alt=""
         className="select-none absolute -bottom-32 -left-12"
       />
-<div>
-
-            </div>
-     
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
